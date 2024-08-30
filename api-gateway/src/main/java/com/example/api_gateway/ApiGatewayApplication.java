504c284088a4dce54c2f02572d87586b4d46480b
erikyuzwa <erikyuzwa@gmail.com>
2024-08-30 08:59:19 -0600

    updated other services

--- api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.example.weapon_microservice;
+package com.example.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class WeaponMicroserviceApplication {
+public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WeaponMicroserviceApplication.class, args);
+		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
 
 }
